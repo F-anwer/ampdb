08b3322db890bd54e6e26e0bf0f816724d6b89aa
nadzhou <ahmaddnavid@gmail.com>
2021-07-10 21:46:13 +0500

    fixed

--- am_db/ampdb/models.py
+++ am_db/ampdb/models.py
@@ -12,3 +12,20 @@ class PDBQuery(models.Model):
     def __str__(self):
         return self.query_id
     
+
+class Proteins(models.Model):
+    name = models.CharField(("Name"), max_length=255)
+    sequence = models.CharField(("Sequence"), max_length=500)
+    molecular_weight = models.FloatField(("MolecularMolecularWeight"))
+    length = models.IntegerField(("Length"))
+    charge = models.FloatField(("Charge"))
+    p_i = models.FloatField(("pI"))
+    a_index = models.FloatField(("AIndex"))
+    instaIndex = models.FloatField(("InstaIndex"))
+    BomanIndex = models.FloatField(("BomanIndex"))
+    hydrophobicity = models.FloatField(("Hydrophobicity"))
+    hmoment_angle = models.FloatField(("MomentAngle"))
+    hmoment_2 = models.FloatField(("Moment2"))
+    pdb_id = models.CharField(("PDBID"), max_length=256)
+
+
--- am_db/ampdb/views.py
+++ am_db/ampdb/views.py
@@ -14,28 +14,25 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         pass
     
-        # def search(self, request):
-    #     if request.method == 'GET':
-    #         search_id = request.GET.get('textfield', None)
-    #         try:
-    #             query = PDBQuery.objects.get(name = query_id)
-    #             html = ("<H1>%s</H1>", query)
-    #             return HttpResponse(html)
-    #         except PDBQuery.DoesNotExist:
-    #             return HttpResponse("no such protein available")  
-    #     else:
-    #         return render(request, 'ampdb/index.html')
-
-
 
 class ResultsView(generic.DetailView):
     model = PDBQuery
     template_name = 'ampdb/results.html'
-    context_object_name = 'query_id'
+    context = get_context_data() 
 
     def get_context_data(self, **kwargs):
-        # Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
-        # Add in a QuerySet of all the books
-        context['book_list'] = PDBQuery.objects.all()
+        context['system'] = self.request.GET.get('system', None)
         return context
+
+
+        # def search(self):
+        #     if request.method == 'GET':
+        #     try:
+        #         query = PDBQuery.objects.get(name = query_id)
+        #         if query_id == search_id: 
+        #             return get_object_or_404(html)
+        #     except PDBQuery.DoesNotExist:
+        #         return HttpResponse("no such protein available")  
+        # else:
+        #     return render(request, 'ampdb/index.html')
