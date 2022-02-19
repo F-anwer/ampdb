1c0b609ca552c829d366e1279295ea651b6fec5c
F-anwer <fanwar.phdabs17asab@student.nust.edu.pk>
2022-02-19 16:01:41 +0500

    first commit

--- am_db/settings.py
+++ am_db/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -148,3 +149,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 ]
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
