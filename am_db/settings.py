c945002775697126bff248cc4672659cb2d425e8
F-anwer <fanwar.phdabs17asab@student.nust.edu.pk>
2022-02-27 22:15:57 +0500

    secret key chng

--- am_db/settings.py
+++ am_db/settings.py
@@ -23,12 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-<<<<<<< HEAD
 
 # SECRET_KEY = "django-insecure-iazodh9$*0bg6vise2x80n$d%ciq=24u+($=@_4@pfj_qzrm0c"
-
-=======
->>>>>>> farha
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
